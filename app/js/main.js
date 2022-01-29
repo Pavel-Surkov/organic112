2fd1ec83246dbf6abc179cedc889532a20a87a63
Pavel Surkov <surkovv.pavel@gmail.com>
2022-01-29 23:42:42 +0700

    adds home ain block

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
+const homeSwiperConfig = {
+	autoplay: {
+		delay: 3500
+	},
+	pagination: {
+		el: '.home-main__swiper .swiper-pagination'
+	}
+};
+
+const homeSwiper = new Swiper('.home-main__swiper', homeSwiperConfig);
+
 document.addEventListener(`DOMContentLoaded`, () => {});
 
 // JQUERY
